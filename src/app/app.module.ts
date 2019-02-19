8053013fb5f76ce25d8f761bccc7f62c5ef62ea5
Francomalu <franco.m.2@hotmail.es>
2019-02-18 22:34:17 -0300

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,31 @@
 import { BrowserModule } from '@angular/platform-browser';
+
+
+// Modulos
 import { NgModule } from '@angular/core';
+import { PagesModule } from './pages/pages.module';
 
 
 import { AppComponent } from './app.component';
 
+import { APP_ROUTES } from './app.routes';
+
+
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './login/register.component';
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegisterComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    APP_ROUTES,
+    PagesModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/pages/pages.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { PagesComponent } from './pages.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { SharedModule } from '../shared/shared.module';
+import { PAGES_ROUTES } from './pages.routes';
+
+
+@NgModule({
+    declarations: [
+        PagesComponent,
+        DashboardComponent
+    ],
+    exports: [
+        PagesComponent,
+        DashboardComponent
+    ],
+    imports: [
+        SharedModule,
+        PAGES_ROUTES
+    ]
+})
+
+export class PagesModule { }
